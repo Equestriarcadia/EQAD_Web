10f24661d64c40267d4a4ca35df2d7d7dbdcdc82
JessDaodao <694428809@qq.com>
2025-07-24 09:22:42 +0800

    删除大量无用css和js，并添加wiki侧边导航栏滚动高亮

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,7 +4,6 @@
    Description: Custom JS file
 */
 
-
 (function($) {
     "use strict";
 
@@ -19,7 +18,6 @@
         '网站基于模板二次创作，已在GitHub开源：https://github.com/Equestriarcadia/EQAD_Web'
 	);
 
-	/* Preloader */
 	$(window).on('load', function() {
 		var preloaderFadeOutTime = 500;
 		function hidePreloader() {
@@ -31,9 +29,6 @@
 		hidePreloader();
 	});
 
-	
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
 		if ($(".navbar").offset().top > 20) {
 			$(".fixed-top").addClass("top-nav-collapse");
@@ -42,7 +37,6 @@
 		}
     });
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
 			var $anchor = $(this);
@@ -53,94 +47,23 @@
 		});
 	});
 
-    // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
     if (!$(this).parent().hasClass('dropdown'))
         $(".navbar-collapse").collapse('hide');
     });
 
-
-    /* Rotating Text - Morphtext */
 	$("#js-rotating").Morphext({
-		// The [in] animation type. Refer to Animate.css for a list of available animations.
 		animation: "fadeIn",
-		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
 		separator: ",",
-		// The delay between the changing of each phrase in milliseconds.
 		speed: 2000,
 		complete: function () {
 			// Called after the entrance animation is executed.
 		}
     });
     
-
-    /* Card Slider - Swiper */
-	var cardSlider = new Swiper('.card-slider', {
-		autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-		},
-        loop: true,
-        navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev'
-		},
-		slidesPerView: 3,
-		spaceBetween: 20,
-        breakpoints: {
-            // when window is <= 992px
-            992: {
-                slidesPerView: 2
-            },
-            // when window is <= 768px
-            768: {
-                slidesPerView: 1
-            } 
-        }
-    });
-
-    
-    /* Lightbox - Magnific Popup */
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-    });
-    
-
-    /* Filter - Isotope */
-    var $grid = $('.grid').isotope({
-        // options
-        itemSelector: '.element-item',
-        layoutMode: 'fitRows'
-    });
-    
-    // filter items on button click
-    $('.filters-button-group').on( 'click', 'a', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-    
-    // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'a', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-        });	
-    });
-    
-
-    /* Counter - CountTo */
 	var a = 0;
 	$(window).scroll(function() {
-		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
+		if ($('#counter').length) {
 			var oTop = $('#counter').offset().top - window.innerHeight;
 			if (a == 0 && $(window).scrollTop() > oTop) {
 			$('.counter-value').each(function() {
@@ -159,7 +82,6 @@
 					},
 					complete: function() {
 					$this.text(this.countNum);
-					//alert('finished');
 					}
 				});
 			});
@@ -168,9 +90,35 @@
 		}
     });
 
-	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
+	});
+
+	document.addEventListener('DOMContentLoaded', function() {
+    	const navLinks = document.querySelectorAll('.sidebar-nav-menu a.page-scroll');
+    
+    	window.addEventListener('scroll', function() {
+    	    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        
+    	    navLinks.forEach(link => {
+    	        const targetId = link.getAttribute('href').substring(1);
+    	        const targetElement = document.getElementById(targetId);
+            
+    	        if (targetElement) {
+    	            const targetPosition = targetElement.offsetTop;
+    	            const targetHeight = targetElement.offsetHeight;
+                
+    	            if (scrollPosition >= targetPosition - 149 && 
+    	                scrollPosition < targetPosition + targetHeight - 100) {
+    	                link.classList.add('active');
+    	            } else {
+    	                link.classList.remove('active');
+    	            }
+    	        }
+    	    });
+    	});
+    
+    	window.dispatchEvent(new Event('scroll'));
 	});
 
 })(jQuery);
